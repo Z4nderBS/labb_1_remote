2354800997c2605217598a0879ec75a525e35970
Zander <alexander.backlin.salas@iths.se>
2022-09-13 13:33:22 +0200

    la in kod från program.cs i visual studio

---
+++ ConsoleApp1/ConsoleApp1/labb_1.cs
@@ -0,0 +1,42 @@
+﻿//================================================
+// Mat-Mats Logistik v. 1.3
+//================================================
+
+bool isRunning = true;
+
+while (isRunning)
+{
+    DisplayMenu();
+
+    ReadInput();
+}
+
+void DisplayMenu()
+{
+
+    Console.WriteLine("======MAT-MATS LOGISTIK======\n1. Registrera ny leverans\n2. Beställ mer\n3. Ny inventering\n4. Stäng");
+    Console.Write(">");
+
+}
+
+void ReadInput()
+{
+    switch (Int32.Parse(Console.ReadLine()))
+    {
+        case 1:
+            Console.WriteLine("Skönt. Då slipper vi ringa och klaga på att varorna inte levereras i tid.");
+            break;
+        case 2:
+            Console.WriteLine("Hellre mer i lager än för lite...");
+            break;
+        case 3:
+            Console.WriteLine("Vad väntar du på? Börja inventera hyllorna!");
+            break;
+        case 4:
+            isRunning = false;
+            Console.WriteLine("Bra jobbat. Ses imorgon.");
+            break;
+        default:
+            break;
+    }
+}
