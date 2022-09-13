bf62e5f75bf14192bb20bdce3c601336a4874666
Zander <alexander.backlin.salas@iths.se>
2022-09-13 13:46:26 +0200

    la in en try-catch som ger en felmeddelande om man skriver in en bokstav istället för siffra i ReadInput metoden och gjorde DisplayMenu koden lite finare med strings

--- ConsoleApp1/ConsoleApp1/labb_1.cs
+++ ConsoleApp1/ConsoleApp1/labb_1.cs
@@ -13,30 +13,45 @@ while (isRunning)
 
 void DisplayMenu()
 {
+    string title = "======MAT-MATS LOGISTIK======";
+    string firstLine = "1. Registrera ny leverans";
+    string secondLine = "2. Beställ mer";
+    string thirdLine = "3. Ny inventering";
+    string lastLine = "4. Stäng";
 
-    Console.WriteLine("======MAT-MATS LOGISTIK======\n1. Registrera ny leverans\n2. Beställ mer\n3. Ny inventering\n4. Stäng");
+    Console.WriteLine($"{title}\n{firstLine}\n{secondLine}\n{thirdLine}\n{lastLine}");
     Console.Write(">");
 
 }
 
 void ReadInput()
 {
-    switch (Int32.Parse(Console.ReadLine()))
+   
+    try
     {
-        case 1:
-            Console.WriteLine("Skönt. Då slipper vi ringa och klaga på att varorna inte levereras i tid.");
-            break;
-        case 2:
-            Console.WriteLine("Hellre mer i lager än för lite...");
-            break;
-        case 3:
-            Console.WriteLine("Vad väntar du på? Börja inventera hyllorna!");
-            break;
-        case 4:
-            isRunning = false;
-            Console.WriteLine("Bra jobbat. Ses imorgon.");
-            break;
-        default:
-            break;
+        switch (Int32.Parse(Console.ReadLine()))
+        {
+            case 1:
+                Console.WriteLine("Skönt. Då slipper vi ringa och klaga på att varorna inte levereras i tid.");
+                break;
+            case 2:
+                Console.WriteLine("Hellre mer i lager än för lite...");
+                break;
+            case 3:
+                Console.WriteLine("Vad väntar du på? Börja inventera hyllorna!");
+                break;
+            case 4:
+                isRunning = false;
+                Console.WriteLine("Bra jobbat. Ses imorgon.");
+                break;
+            default:
+                break;
+        }
     }
+    catch (Exception)
+    {
+        string error = "Error: du skrev inte in ett nummer";
+        Console.WriteLine(error);
+    }
+   
 }
